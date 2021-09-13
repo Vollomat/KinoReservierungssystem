65fda96bafe254021912225605c74e33e8c377f2
Vollomat <patrick.vollstedt@gmx.de>
2021-09-13 17:55:07 +0200

    Erweiterung UML Klassendiagramm

---
+++ src/main/java/com/example/demo/javaklassen/Mitarbeiter.java
@@ -0,0 +1,79 @@
+package com.example.demo.javaklassen;
+
+public class Mitarbeiter {
+
+    private static int zaehler = 0;
+    private int mitarbeiterID;
+    private final static boolean ADMINRECHTE = false;
+    private String iban;
+    private String bic;
+    private String rentenVersicherungsnummer;
+    private int steuerID;
+    private int gehaltsstufe;
+
+    public Mitarbeiter(String iban, String bic, String rentenVersicherungsnummer, int steuerID, int gehaltsstufe) {
+        this.mitarbeiterID = zaehler + 1;
+        this.iban = iban;
+        this.bic = bic;
+        this.rentenVersicherungsnummer = rentenVersicherungsnummer;
+        this.steuerID = steuerID;
+        this.gehaltsstufe = gehaltsstufe;
+        zaehler++;
+    }
+
+    public static int getZaehler() {
+        return zaehler;
+    }
+
+    public static void setZaehler(int zaehler) {
+        Mitarbeiter.zaehler = zaehler;
+    }
+
+    public int getMitarbeiterID() {
+        return mitarbeiterID;
+    }
+
+    public void setMitarbeiterID(int mitarbeiterID) {
+        this.mitarbeiterID = mitarbeiterID;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public String getBic() {
+        return bic;
+    }
+
+    public void setBic(String bic) {
+        this.bic = bic;
+    }
+
+    public String getRentenVersicherungsnummer() {
+        return rentenVersicherungsnummer;
+    }
+
+    public void setRentenVersicherungsnummer(String rentenVersicherungsnummer) {
+        this.rentenVersicherungsnummer = rentenVersicherungsnummer;
+    }
+
+    public int getSteuerID() {
+        return steuerID;
+    }
+
+    public void setSteuerID(int steuerID) {
+        this.steuerID = steuerID;
+    }
+
+    public int getGehaltsstufe() {
+        return gehaltsstufe;
+    }
+
+    public void setGehaltsstufe(int gehaltsstufe) {
+        this.gehaltsstufe = gehaltsstufe;
+    }
+}
