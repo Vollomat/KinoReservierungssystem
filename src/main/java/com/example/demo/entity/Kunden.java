22dc970ac4a5995709cb0b6d807b2bb26a6ac285
Vollomat <patrick.vollstedt@gmx.de>
2021-09-28 09:49:28 +0200

    Neues Frontend

--- src/main/java/com/example/demo/entity/Kunden.java
+++ src/main/java/com/example/demo/entity/Kunden.java
@@ -17,6 +17,24 @@ public class Kunden {
 
    private String nachname;
 
+   private String email;
+
+   private int alterInJahren;
+
+   private String strasse;
+
+   private String hausnummer;
+
+   private String postleitzahl;
+
+   private String ort;
+
+   private String username;
+
+   private String passwort;
+
+
+
 
     public int getId() {
         return id;
@@ -42,5 +60,67 @@ public class Kunden {
         this.nachname = nachname;
     }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswort() {
+        return passwort;
+    }
+
+    public void setPasswort(String passwort) {
+        this.passwort = passwort;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAlterInJahren() {
+        return alterInJahren;
+    }
+
+    public void setAlterInJahren(int alterInJahren) {
+        this.alterInJahren = alterInJahren;
+    }
+
+    public String getStrasse() {
+        return strasse;
+    }
+
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+    }
+
+    public String getHausnummer() {
+        return hausnummer;
+    }
+
+    public void setHausnummer(String hausnummer) {
+        this.hausnummer = hausnummer;
+    }
+
+    public String getPostleitzahl() {
+        return postleitzahl;
+    }
+
+    public void setPostleitzahl(String postleitzahl) {
+        this.postleitzahl = postleitzahl;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
 }
