06c6708dbdd352cf39910dd561ddf72c2ae6c59a
Vollomat <patrick.vollstedt@gmx.de>
2021-10-19 22:30:51 +0200

    Tests hinzugefügt (57% Coverage)

--- src/test/java/com/example/demo/controller/BestellungenControllerTest.java
+++ src/test/java/com/example/demo/controller/BestellungenControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -49,13 +48,18 @@ class BestellungenControllerTest {
 
     @Test
     void emailSenden() {
+        //TODO
     }
 
     @Test
     void preisBerechnenAllerTicketsDerBuchung() {
+        //TODO
     }
 
     @Test
     void sitzplatzStatusAufGebuchtSetzen() {
+        //TODO
     }
+
+
 }
